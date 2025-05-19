4a9e225508d993b5de3b7a064f7ad04399aa09d2
Tayler <tayler.westercamp@gmail.com>
2025-05-19 18:55:54 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import About from "./Components/About"
+import Contact from "./Components/Contact"
+import Features from "./Components/Features"
+import Footer from "./Components/Footer"
+import Hero from "./Components/Hero"
+import NavBar from "./Components/Navbar"
+import Story from "./Components/Story"
+
+
+const App = () => {
+  return (
+    <main className="relative min-h-screen w-screen overflow-x-hidden">
+      <NavBar />
+      <Hero />
+      <About />
+      <Features />
+      <Story />
+      <Contact />
+      <Footer />
+    </main>
+  )
+}
+
+export default App
